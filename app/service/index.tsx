d0bc5d27e6d1a6e61ac678ce75e40f165cc80de1
Vy Nguyen <nguyenluminhvy@gmail.com>
2026-04-28 19:57:39 +0700

    service add search bar

--- app/service/index.tsx
+++ app/service/index.tsx
@@ -1,18 +1,25 @@
-import {RefreshControl, StyleSheet} from 'react-native';
+import {RefreshControl, StyleSheet, TouchableOpacity} from 'react-native';
 
 import { View } from '@/components/Themed';
 import {FlashList} from "@shopify/flash-list";
-import {Button} from "react-native-paper";
+import {Button, MD3Colors, Text} from "react-native-paper";
 import {router, Stack} from "expo-router";
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {useSpa} from "@/lib/context/SpaContext";
 import {ServiceItem} from "@/lib/components/ui/ServiceItem";
-import {Ionicons} from "@expo/vector-icons";
+import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
+import _ from "lodash";
+import {AppTextInput} from "@/lib/components/ui/AppTextInput";
+import {Image} from "expo-image";
+import {IMAGES} from "@/lib/assets/images";
 
 export default function ServicesScreen() {
   const { fetchServices, services } = useSpa()
   const [refreshing, setRefreshing] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
+  const searchInputRef = useRef<any>(null);
+  const listRef = useRef<any>(null);
 
   useEffect(() => {
     ;(async () => {
@@ -32,6 +39,35 @@ export default function ServicesScreen() {
     router.push(`/service/${item.id}`);
   }
 
+  const debouncedSearch = useMemo(
+    () => _.debounce((text: string) => setSearchText(text), 300),
+    []
+  );
+
+  const handleSearch = (value: string) => {
+    debouncedSearch(value);
+  };
+
+  const dataFilter = useMemo(() => {
+    setTimeout(() => {
+      listRef.current?.scrollToOffset({
+        offset: 0,
+        animated: false,
+      });
+    }, 200)
+
+    if (!searchText) return services;
+
+    const keyword = searchText.toLowerCase();
+
+    return services?.filter((s: any) =>
+      s?.name?.toLowerCase().includes(keyword) ||
+      s?.description?.toLowerCase().includes(keyword) ||
+      String(s?.price).includes(keyword) ||
+      String(s?.duration).includes(keyword)
+    );
+  }, [searchText, services]);
+
   return (
     <View style={styles.container}>
       <Stack.Screen
@@ -43,11 +79,31 @@ export default function ServicesScreen() {
         }}
       />
 
+      <View style={styles.searchContainer}>
+        <AppTextInput
+          ref={searchInputRef}
+          showSearchIcon
+          placeholder="Search..."
+          onChangeText={handleSearch}
+          RightComponent={searchText?.length > 0 && <TouchableOpacity style={{ paddingRight: 8}} onPress={() => {
+            handleSearch('')
+            searchInputRef.current?.clear();
+          }}>
+            <MaterialCommunityIcons
+              name={"close-circle"}
+              size={20}
+              color={MD3Colors.neutralVariant60}
+            />
+          </TouchableOpacity>}
+        />
+      </View>
+
       <FlashList
+        ref={listRef}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ padding: 16, paddingBottom: 80}}
         keyExtractor={(item) => item.id.toString()}
-        data={services}
+        data={dataFilter}
         renderItem={({ item }) => <ServiceItem item={item} onPress={() => viewServiceDetails(item)} />}
         refreshControl={
           <RefreshControl
@@ -55,6 +111,24 @@ export default function ServicesScreen() {
             onRefresh={onRefresh}
           />
         }
+        ListEmptyComponent={<View style={{
+          flex: 1,
+          paddingTop: 100,
+          alignItems: 'center',
+          gap: 8
+        }}>
+          <Image
+            style={{
+              width: "100%",
+              height: 50,
+            }}
+            source={IMAGES.nodata}
+            contentFit="contain"
+          />
+          <Text variant={'labelMedium'}>
+            No data
+          </Text>
+        </View>}
       />
     </View>
   );
@@ -96,5 +170,17 @@ const styles = StyleSheet.create({
     right: 16,
     position: "absolute",
     backgroundColor: "#006EE9",
+  },
+
+  searchContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  clearButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: 8,
   },
 });
